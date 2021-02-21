f4be7db0a1d103bfd1b94903d260b25bd1b5cfa0
Marie <mariela.gutierrez2906@gmail.com>
2021-02-21 19:52:29 +0200

    End of section 11.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,12 @@ using System.Threading.Tasks;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
+using API.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,9 +21,11 @@ namespace API.Controllers
     {
         private readonly IUserRepository userRepository;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository  userRepository, IMapper mapper)
+        public UsersController(IUserRepository  userRepository, IMapper mapper, IPhotoService photoService)
         {
+            this._photoService = photoService;
             this._mapper = mapper;
             this.userRepository = userRepository;
         }
@@ -36,7 +41,7 @@ namespace API.Controllers
         }
 
         //To get an specific user e.g api/user/3
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name="GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username){
            
          return await userRepository.GetMemberAsync(username);
@@ -48,8 +53,7 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto){
 
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user= await userRepository.GetUserByUsernameAsync(username);
+            var user= await userRepository.GetUserByUsernameAsync(User.GetUsername());
 
             _mapper.Map(memberUpdateDto,user);
 
@@ -59,5 +63,82 @@ namespace API.Controllers
 
            return BadRequest("Failed to update user profile");
         }
+
+        [HttpPost("add-photo")]
+
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file){
+
+           var user= await userRepository.GetUserByUsernameAsync(User.GetUsername());
+           
+           var result= await _photoService.AddPhotoAsync(file);
+
+           if(result.Error != null)return BadRequest(result.Error.Message);
+
+           var photo = new Photo{
+               Url = result.SecureUrl.AbsoluteUri,
+               PublicId = result.PublicId
+           };
+           if(user.Photos.Count == 0){
+               photo.IsMain=true;
+           }
+
+            user.Photos.Add(photo);
+
+            if(await userRepository.SaveAllAsync()){
+
+               //return _mapper.Map<PhotoDto>(photo);//Return the photo
+               return CreatedAtRoute("GetUser",new {username = user.UserName},_mapper.Map<PhotoDto>(photo));
+            }
+             
+
+              return BadRequest("Problem adding photo");
+
+        }
+
+        [HttpPut("set-main-photo/{photoId}")]
+
+        public async Task<ActionResult> SetMainPhoto(int photoId)
+        {
+            var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo= user.Photos.FirstOrDefault(x=>x.Id == photoId);
+
+            if (photo.IsMain) return BadRequest("This is already your main photo");
+             
+            var currentMain = user.Photos.FirstOrDefault(x=>x.IsMain);
+
+            if(currentMain != null) currentMain.IsMain=false;//turn off the old main
+            photo.IsMain=true; //set the new main
+
+            if(await userRepository.SaveAllAsync()) return NoContent();
+
+            return BadRequest("Failed to set main photo");
+ 
+        }
+
+        [HttpDelete("delete-photo/{photoId}")]
+        public async Task<ActionResult> DeletePhoto(int photoId){
+
+            var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+            var photo= user.Photos.FirstOrDefault(x=>x.Id == photoId);
+
+            if(photo == null) return NotFound();
+
+            if(photo.IsMain) return BadRequest("You cannot delete your main photo");
+
+            if(photo.PublicId != null){
+
+                var result = await _photoService.DeletePhotoAsync(photo.PublicId);
+                if(result.Error != null) return BadRequest(result.Error.Message);
+            }
+
+            user.Photos.Remove(photo);
+
+            if(await userRepository.SaveAllAsync()) return Ok();
+
+            return  BadRequest("Failed to delete photo");
+        }
+
     }
 }
---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,83 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { take } from 'rxjs/operators';
+import { Member } from 'src/app/_models/member';
+import { Photo } from 'src/app/_models/photo';
+import { User } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css']
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input()
+  member!: Member;
+  hasBaseDropZoneOver: boolean = false;
+  hasAnotherDropZoneOver:any;
+  uploader!: FileUploader;
+  baseUrl= environment.apiUrl;
+  user!: User;
+
+  constructor(private accountService:AccountService, private memberService:MembersService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user=>this.user=user)
+   }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e:any){
+    this.hasBaseDropZoneOver=e;
+  }
+
+ setMainPhoto(photo:Photo){
+    this.memberService.setMainPhoto(photo.id).subscribe(()=>{
+      this.user.photoUrl = photo.url;
+      this.accountService.setCurrentUser(this.user);
+      this.member.photoUrl = photo.url;
+       //Update the main photo
+       this.member.photos.forEach(p=>{
+         if(p.isMain) p.isMain=false;
+         if(p.id === photo.id) p.isMain=true;
+       })
+
+    })
+ }
+
+ deletePhoto(photoId:Number){
+   this.memberService.deletePhoto(photoId).subscribe(()=>{
+      this.member.photos = this.member.photos.filter(x=>x.id !== photoId);
+   })
+
+ }
+  initializeUploader(){
+    this.uploader   = new FileUploader({
+      url: this.baseUrl+'users/add-photo',
+      authToken: 'Bearer '+this.user.token,
+      isHTML5:true,
+      allowedFileType:['image'],
+      removeAfterUpload:true,
+      autoUpload:false,
+      maxFileSize:10*1024*1024
+    }); 
+
+    this.uploader.onAfterAddingFile = (file)=>{
+      file.withCredentials=false;//otherwise credentials will be required
+    }
+
+    this.uploader.onSuccessItem=(item: any, response: string)=>{
+
+      if(response){
+        const photo =JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    }
+  }
+
+ 
+
+}
